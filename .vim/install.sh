1cb62519ceb0c973445f417632726054ab269f89
dodo5522 <dodo5522@284ab01a-e931-415e-a06d-2cba5beede73>
2014-10-06 03:20:50 +0000

    Add sudo to link.

--- .vim/install.sh
+++ .vim/install.sh
@@ -11,5 +11,5 @@ if ! [ -d ./plantuml-syntax ]; then
 	git clone https://github.com/aklt/plantuml-syntax.git ./plantuml-syntax
 	ln -fs ../plantuml-syntax/ftdetect/plantuml.vim ./ftdetect
 	ln -fs ../plantuml-syntax/ftplugin/plantuml.vim ./ftplugin
-	ln -fs ${PWD}/plantuml-syntax/syntax/plantuml.vim /usr/share/vim/vim74/syntax
+	sudo ln -fs ${PWD}/plantuml-syntax/syntax/plantuml.vim /usr/share/vim/vim74/syntax
 fi
