65127ad07703f2c58f0cd272e41f4bbafa4c8a9a
dodo5522 <dodo5522@284ab01a-e931-415e-a06d-2cba5beede73>
2014-10-06 07:40:01 +0000

    To use neobundle for plantuml-syntax.

--- .vim/install.sh
+++ .vim/install.sh
@@ -6,10 +6,3 @@ if ! [ -d ./bundle/neobundle.vim ]; then
         git clone http://github.com/Shougo/neobundle.vim
     fi
 fi
-
-if ! [ -d ./plantuml-syntax ]; then
-	git clone https://github.com/aklt/plantuml-syntax.git ./plantuml-syntax
-	ln -fs ../plantuml-syntax/ftdetect/plantuml.vim ./ftdetect
-	ln -fs ../plantuml-syntax/ftplugin/plantuml.vim ./ftplugin
-	sudo ln -fs ${PWD}/plantuml-syntax/syntax/plantuml.vim /usr/share/vim/vim74/syntax
-fi
