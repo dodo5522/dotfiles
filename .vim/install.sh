eca240c19332350fd7928eb9f48186283dede972
dodo5522 <dodo5522@284ab01a-e931-415e-a06d-2cba5beede73>
2014-10-06 03:18:41 +0000

    Add syntax highlight rule for plantuml file.

--- .vim/install.sh
+++ .vim/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 if ! [ -d ./bundle/neobundle.vim ]; then
     mkdir -p ./bundle
     cd ./bundle
@@ -7,3 +7,9 @@ if ! [ -d ./bundle/neobundle.vim ]; then
     fi
 fi
 
+if ! [ -d ./plantuml-syntax ]; then
+	git clone https://github.com/aklt/plantuml-syntax.git ./plantuml-syntax
+	ln -fs ../plantuml-syntax/ftdetect/plantuml.vim ./ftdetect
+	ln -fs ../plantuml-syntax/ftplugin/plantuml.vim ./ftplugin
+	ln -fs ${PWD}/plantuml-syntax/syntax/plantuml.vim /usr/share/vim/vim74/syntax
+fi
