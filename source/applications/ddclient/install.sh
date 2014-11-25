16c7edfdfc7c82a50b36f4554a473181d2396e45
dodo5522 <dodo5522@284ab01a-e931-415e-a06d-2cba5beede73>
2014-11-25 23:13:22 +0000

    [ddclient] not copy conf file if exist on /etc.

--- source/applications/ddclient/install.sh
+++ source/applications/ddclient/install.sh
@@ -3,5 +3,5 @@
 cp ddclient /usr/sbin/ddclient
 cp sample-etc_rc.d_init.d_ddclient.ubuntu /etc/init.d/ddclient
 mkdir -p /etc/ddclient/
-cp -f sample-etc_ddclient.conf /etc/ddclient/ddclient.conf
+[ -e /etc/ddclient/ddclient.conf ] || cp -f sample-etc_ddclient.conf /etc/ddclient/ddclient.conf
 mkdir -p /var/cache/ddclient
