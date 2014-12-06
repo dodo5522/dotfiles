8e38b91547027d730090b6a565b2a5bebd2e7cce
dodo5522 <dodo5522@284ab01a-e931-415e-a06d-2cba5beede73>
2014-12-06 16:58:55 +0000

    Remove ddclient

--- source/applications/ddclient/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp ddclient /usr/sbin/ddclient
-cp sample-etc_rc.d_init.d_ddclient.ubuntu /etc/init.d/ddclient
-mkdir -p /etc/ddclient/
-[ -e /etc/ddclient/ddclient.conf ] || cp -f sample-etc_ddclient.conf /etc/ddclient/ddclient.conf
-mkdir -p /var/cache/ddclient
